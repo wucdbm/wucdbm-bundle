59b5de8bc24b296a14e979d1935fa7e644545146
Martin Kirilov <wucdbm@gmail.com>
2015-01-24 02:10:11 +0200

    composer.json

---
+++ Wucdbm/WucdbmBundle/DependencyInjection/WucdbmWucdbmExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wucdbm\LyrixBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class WucdbmWucdbmExtension extends Extension {
+
+    public function load(array $configs, ContainerBuilder $container) {
+        $config = array();
+        foreach ($configs as $subConfig) {
+            $config = array_merge($config, $subConfig);
+        }
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('core.xml');
+
+    }
+
+    public function getXsdValidationBasePath() {
+        return __DIR__ . '/../Resources/config/';
+    }
+
+    public function getNamespace() {
+        return 'http://www.example.com/symfony/schema/';
+    }
+
+}
