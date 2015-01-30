ce922d3bd59efe4af65566b23622280f64b4f10e
Martin Kirilov <wucdbm@gmail.com>
2015-01-31 01:20:17 +0200

    First commit

---
+++ Controller/BaseController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wucdbm\Bundle\WucdbmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+abstract class BaseController extends Controller {
+
+    public function json($data = null, $status = 200, $headers = array()) {
+        return new JsonResponse($data, $status, $headers);
+    }
+
+    public function witter($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('witter' => $data), $status, $headers);
+    }
+
+    public function replace($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('replace' => $data), $status, $headers);
+    }
+
+    public function append($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('append' => $data), $status, $headers);
+    }
+
+    public function prepend($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('prepend' => $data), $status, $headers);
+    }
+
+    public function mfp($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('mfp' => $data), $status, $headers);
+    }
+
+    public function injectHtml($data = null, $status = 200, $headers = array()) {
+        return $this->json(array('injectHtml' => $data), $status, $headers);
+    }
+
+}
