a0bc3faacaf66f96d65179b4b5307b2557f20ebb
Martin Kirilov <wucdbm@gmail.com>
2015-06-09 19:25:37 +0300

    add option to disable limit without having to do ->remove('limit');

--- Form/Filter/BasicFilterType.php
+++ Form/Filter/BasicFilterType.php
@@ -15,38 +15,36 @@ class BasicFilterType extends AbstractType {
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $this->addLimitField($builder);
+        if ($options['enable_limit']) {
+            $builder->add('limit', 'choice', array(
+                'choices' => array(
+                    0    => 'All',
+                    10   => '10 Results',
+                    20   => '20 Results',
+                    50   => '50 Results',
+                    100  => '100 Results',
+                    250  => '250 Results',
+                    500  => '500 Results',
+                    1000 => '1000 Results'
+                ),
+                'label'   => 'Number of items',
+                'attr'    => array(
+                    'class' => 'select2'
+                )
+            ));
 
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use ($builder) {
-            // This is to make sure that if the limit was not passed in the request,
-            // the default value of the model would be used and shown when the form is rendered,
-            // instead of showing 'All' in the select, but limiting to the default instead
-            $data = $event->getData();
-            if (!isset($data['limit'])) {
-                $form = $event->getForm();
-                $data['limit'] = $form->get('limit')->getData();
-                $event->setData($data);
-            }
-        });
-    }
-
-    protected function addLimitField(FormBuilderInterface $builder) {
-        $builder->add('limit', 'choice', array(
-            'choices' => array(
-                0    => 'All',
-                10   => '10 Results',
-                20   => '20 Results',
-                50   => '50 Results',
-                100  => '100 Results',
-                250  => '250 Results',
-                500  => '500 Results',
-                1000 => '1000 Results'
-            ),
-            'label'   => 'Number of items',
-            'attr'    => array(
-                'class' => 'select2'
-            )
-        ));
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use ($builder) {
+                // This is to make sure that if the limit was not passed in the request,
+                // the default value of the model would be used and shown when the form is rendered,
+                // instead of showing 'All' in the select, but limiting to the default instead
+                $data = $event->getData();
+                if (!isset($data['limit'])) {
+                    $form = $event->getForm();
+                    $data['limit'] = $form->get('limit')->getData();
+                    $event->setData($data);
+                }
+            });
+        }
     }
 
     /**
@@ -59,7 +57,8 @@ class BasicFilterType extends AbstractType {
             ),
             'method'             => 'GET',
             'csrf_protection'    => false,
-            'allow_extra_fields' => true
+            'allow_extra_fields' => true,
+            'enable_limit' => true
         ));
     }
 
