6737a4369bf9931bce69bc8b509f263a57342227
Martin Kirilov <wucdbm@gmail.com>
2015-07-21 20:26:54 +0300

    array extension - chunk array

---
+++ Twig/ArrayExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wucdbm\Bundle\WucdbmBundle\Twig;
+
+use Doctrine\Common\Collections\Collection;
+
+class ArrayExtension extends \Twig_Extension {
+
+    public function getFilters() {
+        return array(
+            'array_chunk' => new \Twig_Filter_Method($this, 'array_chunk')
+        );
+    }
+
+    public function array_chunk($array, $size, $preserve_key = false) {
+        if (is_array($array)) {
+            return array_chunk($array, $size, $preserve_key);
+        }
+        if ($array instanceof Collection) {
+            $ret = [];
+            $key = 0;
+            while (count($slice = $array->slice($key*$size, $size))) {
+                $ret[$key++] = $slice;
+            }
+            return $ret;
+        }
+    }
+
+    public function getName() {
+        return 'wucdbm_array';
+    }
+
+    public function getAlias() {
+        return 'wucdbm_array';
+    }
+}
