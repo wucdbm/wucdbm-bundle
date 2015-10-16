db53cf8c77e48782e280fc6596124658c9a9f6b4
Martin Kirilov <wucdbm@gmail.com>
2015-10-17 01:30:57 +0300

    implode filter

--- Twig/SumExtension.php
+++ Twig/SumExtension.php
@@ -28,16 +28,38 @@ class SumExtension extends \Twig_Extension {
 
     public function getFilters() {
         return array(
-            'sum'   => new \Twig_Filter_Method($this, 'sum'),
-            'count' => new \Twig_Filter_Method($this, 'count')
+            'implode' => new \Twig_Filter_Method($this, 'implode'),
+            'sum'     => new \Twig_Filter_Method($this, 'sum'),
+            'count'   => new \Twig_Filter_Method($this, 'count')
         );
     }
 
     public function getFunctions() {
         return array(
-            'sum'   => new \Twig_Function_Method($this, 'sum'),
-            'count' => new \Twig_Function_Method($this, 'count')
+            'implode' => new \Twig_Function_Method($this, 'implode'),
+            'sum'     => new \Twig_Function_Method($this, 'sum'),
+            'count'   => new \Twig_Function_Method($this, 'count')
         );
+    }
+
+    public function implode($arrayOrObject, $glue, $propertyPath = null, $expression = null) {
+        $pieces = [];
+        foreach ($arrayOrObject as $value) {
+            $shouldImplode = true;
+            if (null !== $expression) {
+                $shouldImplode = $this->language->evaluate($expression, array(
+                    'value' => $value
+                ));
+            }
+            if ($shouldImplode) {
+                if ($propertyPath) {
+                    $value = $this->accessor->getValue($value, $propertyPath);
+                }
+                $pieces[] = $value;
+            }
+        }
+
+        return implode($glue, $pieces);
     }
 
     public function sum($arrayOrObject, $propertyPath = null, $expression = null) {
@@ -56,6 +78,7 @@ class SumExtension extends \Twig_Extension {
                 $sum += $value;
             }
         }
+
         return $sum;
     }
 
@@ -72,6 +95,7 @@ class SumExtension extends \Twig_Extension {
                 $count++;
             }
         }
+
         return $count;
     }
 
