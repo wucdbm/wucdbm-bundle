7479e7a864b58b03c7b1045d4021f683b5396245
Martin Kirilov <wucdbm@gmail.com>
2015-12-12 20:31:47 +0200

    chocie filter type

--- Form/Filter/ChoiceFilterType.php
+++ Form/Filter/ChoiceFilterType.php
@@ -3,43 +3,35 @@
 namespace Wucdbm\Bundle\WucdbmBundle\Form\Filter;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TextFilterType extends AbstractType {
-
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-
-    }
+class ChoiceFilterType extends AbstractType {
 
     /**
      * @param OptionsResolver $resolver
      */
     public function configureOptions(OptionsResolver $resolver) {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'required'    => false,
             'placeholder' => ''
-        ));
+        ]);
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options) {
-        $view->vars = array_replace_recursive($view->vars, array(
-            'attr' => array(
+        $view->vars = array_replace_recursive($view->vars, [
+            'attr' => [
                 'rel'         => 'tooltip',
                 'title'       => $options['placeholder'],
-                'placeholder' => $options['placeholder']
-            )
-        ));
+                'placeholder' => $options['placeholder'],
+                'class'       => 'select2'
+            ]
+        ]);
     }
 
     public function getParent() {
-        return 'Symfony\Component\Form\Extension\Core\Type\TextType';
+        return 'Symfony\Component\Form\Extension\Core\Type\ChoiceType';
     }
 
 }
