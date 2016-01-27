7a1cb3fe2e87ad9b90b2e0fee6149967aa2cc895
Martin Kirilov <wucdbm@gmail.com>
2016-01-27 17:52:33 +0200

    add silent login

--- Manager/LoginManager.php
+++ Manager/LoginManager.php
@@ -12,6 +12,29 @@ class LoginManager extends AbstractManager {
 
     /**
      * @param AdvancedUserInterface $user
+     * @param $area - This is the firewall NAME
+     * @param $context - This is the firewall CONTEXT
+     */
+    public function loginSilent(AdvancedUserInterface $user, $area, $context) {
+        $stack = $this->container->get('request_stack');
+        $request = $stack->getCurrentRequest();
+
+        if (!$request) {
+            return;
+        }
+
+        $token = new UsernamePasswordToken($user, $user->getPassword(), $area, $user->getRoles());
+
+        $session = $request->getSession();
+        // This one uses the CONTEXT NAME
+        $session->set('_security_'.$context, serialize($token));
+        // This one uses the FIREWALL NAME
+        // This can be used to set the address the user should be redirected to, but is rather useless in this situation?
+//        $session->set('_security.'.$area.'.target_path', 'https://website.com/app_dev.php/some/address');
+    }
+
+    /**
+     * @param AdvancedUserInterface $user
      * @param $area
      */
     public function login(AdvancedUserInterface $user, $area) {
