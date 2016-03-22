3fd089502e0bed68ff32bc9889e92d707cd9bf0a
Martin Kirilov <wucdbm@gmail.com>
2016-03-22 05:01:08 +0200

    url encoded to array filter

--- Twig/ArrayExtension.php
+++ Twig/ArrayExtension.php
@@ -6,11 +6,25 @@ class ArrayExtension extends \Twig_Extension {
 
     public function getFilters() {
         return [
-            new \Twig_SimpleFilter('array_chunk', [$this, 'array_chunk'])
+            new \Twig_SimpleFilter('array_chunk', [$this, 'array_chunk']),
+            new \Twig_SimpleFilter('urlEncodedToArray', [$this, 'urlEncodedToArray'])
         ];
     }
 
-    function array_chunk($input, $size, $preserve_keys = null) {
+    /**
+     * @param $encoded
+     * @return \Generator
+     */
+    public function urlEncodedToArray($encoded) {
+        $array = explode('&', $encoded);
+
+        foreach ($array as $v) {
+            list($key, $value) = explode('=', $v);
+            yield [$key => $value];
+        }
+    }
+
+    public function array_chunk($input, $size, $preserve_keys = null) {
         if ($input instanceof \Traversable) {
             $arr = array();
             foreach ($input as $val) {
